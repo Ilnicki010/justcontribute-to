260dfea054aaf0fb4c12f3fd7f3a948fdfc7be42
Ilnicki010 <dominik.ilnicki@gmail.com>
2020-06-06 22:13:51 +0200

    syntax fix

--- src/constatnts/languages.js
+++ src/constatnts/languages.js
@@ -1,7 +1,7 @@
 const languages = [
   { value: "javascript", label: "Javascript" },
   { value: "python", label: "Python" },
-  { value: "rust", label: "R
+  { value: "rust", label: "Rust" },
   { value: "csharp", label: "C#" },
   { value: "golang", label: "Golang" },
   { value: "java", label: "Java" },
